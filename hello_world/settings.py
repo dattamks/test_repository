8946e7f74fdb87cc0b00e1bde24cf0af54b643f0
Datta mks <66517145+dattamks@users.noreply.github.com>
2023-09-30 18:39:14 +0000

    added railway related files

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -123,11 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    BASE_DIR / "hello_world" / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "hello_world" / "static",
+# ]
 
 STATIC_URL = "static/"
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
